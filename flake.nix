91a47e45043ec38e6cea10bda2b6c40913ae5ed4
Administrator <Administrator@MacBook-Pro-2.local>
2025-05-27 17:14:45 +0700

    Initial commit: flake + CI workflow

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "NixOS for Raspberry Pi 5 (via rpi-nix)";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rpi-nix.url      = "github:nix-community/raspberry-pi-nix";
+  };
+
+  outputs = { self, nixpkgs, rpi-nix, ... }:
+  let
+    system = "aarch64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ rpi-nix.overlays.core ];
+    };
+  in {
+    nixosConfigurations.pi5 = nixpkgs.lib.nixosSystem {
+      inherit system;
+
+      modules = [
+        # Pi hardware + firmware
+        rpi-nix.nixosModules."raspberry-pi"
+        # SD-image builder
+        rpi-nix.nixosModules."sd-image"
+
+        # Your custom settings:
+        ({ config, pkgs, ... }: {
+          # <-- tell it which SoC you have:
+          raspberry-pi-nix.board = "bcm2712";
+
+          networking.hostName       = "my-pi";
+          users.users.pi = {
+            isNormalUser            = true;
+            extraGroups             = [ "wheel" ];
+          };
+          environment.systemPackages = with pkgs; [ vim git ];
+          services.openssh.enable   = true;
+        })
+      ];
+
+      # point nixosSystem at the pkg set we imported
+      pkgs = pkgs;
+    };
+  };
+}
